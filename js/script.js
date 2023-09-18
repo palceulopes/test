c1e8b69c65756b4d9ce7d918663a4aae12115f55
Paulo Alceu <palceu@outlook.com>
2023-09-18 02:22:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+let allHeader = document.querySelector('.flex-container-all-header');
+let pos = 0;
+let style = getComputedStyle(allHeader);
+let left = parseInt(style.left);
+
+addEventListener("resize", (event) => {
+  left = 0;
+  pos = 0;
+  allHeader.style.left = left + "vw";
+});
+
+
+function prev() {
+
+  if (pos > 0) {
+    pos -= 1;
+    left = (left + 100);
+  } else {
+    left = -300;
+    pos = 3;
+  }
+
+  allHeader.style.left = left + "vw";
+  console.log(pos);
+  console.log(left);
+}
+
+function next() {
+
+  if (pos < 3) {
+    pos += 1;
+    left = (left - 100);
+  } else {
+    left = 0;
+    pos = 0;
+  }
+  allHeader.style.left = left + "vw";
+  console.log(pos);
+  console.log(left);
+
+
+}
 
